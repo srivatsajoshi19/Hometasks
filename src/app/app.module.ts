bd1af405c6469a56504fb4e6fdd8e532c678ce17
Srivatsa Joshi <srivatsajoshi19@gmail.com>
2022-01-12 12:07:56 +0530

    modified app.module to include other modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { ActiveModule } from './modules/active/active.module';
+import { DeletedModule } from './modules/deleted/deleted.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, ActiveModule, DeletedModule , NgbModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
